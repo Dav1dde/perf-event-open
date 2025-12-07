bf25fd3edda01ea6158d86474ddfe0d91f8896d3
David Herberth <github@dav1d.de>
2025-12-07 23:02:37 +0100

    add poll_next functions to async iterators

--- src/sample/auxiliary/iter/mod.rs
+++ src/sample/auxiliary/iter/mod.rs
@@ -1,5 +1,7 @@
 use std::io::Result;
 use std::num::NonZeroUsize;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 mod cow;
 
@@ -32,6 +34,20 @@ impl<'a> Iter<'a> {
 pub struct AsyncIter<'a>(AsyncCowIter<'a>);
 
 impl AsyncIter<'_> {
+    /// Attempt to pull out the next value, registering the current task for wakeup if the value is
+    /// not yet available, and returning `None` if the iterator is exhausted.
+    ///
+    /// `max_chunk_len` specifies the maximum length of a chunk
+    /// that can be produced at one time, unlimited if `None`.
+    pub fn poll_next(
+        self: Pin<&mut Self>,
+        max_chunk_len: Option<NonZeroUsize>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Vec<u8>>> {
+        let this = std::pin::Pin::new(&mut self.get_mut().0);
+        this.poll_next(|cc| cc.into_owned(), max_chunk_len, cx)
+    }
+
     /// Advances the iterator and returns the next value.
     ///
     /// `max_chunk_len` specifies the maximum length of a chunk
--- src/sample/iter/mod.rs
+++ src/sample/iter/mod.rs
@@ -1,4 +1,6 @@
 use std::io::Result;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 use super::record::{Priv, Record};
 
@@ -33,6 +35,15 @@ impl Iterator for Iter<'_> {
 pub struct AsyncIter<'a>(AsyncCowIter<'a>);
 
 impl AsyncIter<'_> {
+    /// Attempt to pull out the next value, registering the current task for wakeup if the value is
+    /// not yet available, and returning `None` if the iterator is exhausted.
+    ///
+    /// [`WakeUpOn`][crate::config::WakeUpOn] must be properly set to make this work.
+    pub fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<(Priv, Record)>> {
+        let this = std::pin::Pin::new(&mut self.get_mut().0);
+        this.poll_next(|cc, p| p.parse(cc), cx)
+    }
+
     /// Advances the iterator and returns the next value.
     ///
     /// [`WakeUpOn`][crate::config::WakeUpOn] must be properly set to make this work.
